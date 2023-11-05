ac03887bdcebcada6d6a72304464d88bb2c8d8e3
Andrey <andreonof@yandex.ru>
2023-11-05 12:45:10 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,132 @@
+"use strict";
+//Строгий режим обновления версий
+
+// Аккордион
+class ItcAccordion {
+  constructor(target, config) {
+    this._el =
+      typeof target === "string" ? document.querySelector(target) : target;
+    const defaultConfig = {
+      alwaysOpen: true,
+      duration: 350,
+    };
+    this._config = Object.assign(defaultConfig, config);
+    this.addEventListener();
+  }
+  addEventListener() {
+    this._el.addEventListener("click", (e) => {
+      const elHeader = e.target.closest(".accordion__header");
+      if (!elHeader) {
+        return;
+      }
+      if (!this._config.alwaysOpen) {
+        const elOpenItem = this._el.querySelector(".accordion__item_show");
+        if (elOpenItem) {
+          elOpenItem !== elHeader.parentElement
+            ? this.toggle(elOpenItem)
+            : null;
+        }
+      }
+      this.toggle(elHeader.parentElement);
+    });
+  }
+  show(el) {
+    const elBody = el.querySelector(".accordion__body");
+    if (
+      elBody.classList.contains("collapsing") ||
+      el.classList.contains("accordion__item_show")
+    ) {
+      return;
+    }
+    elBody.style["display"] = "block";
+    const height = elBody.offsetHeight;
+    elBody.style["height"] = 0;
+    elBody.style["overflow"] = "hidden";
+    elBody.style["transition"] = `height ${this._config.duration}ms ease`;
+    elBody.classList.add("collapsing");
+    el.classList.add("accordion__item_slidedown");
+    elBody.offsetHeight;
+    elBody.style["height"] = `${height}px`;
+    window.setTimeout(() => {
+      elBody.classList.remove("collapsing");
+      el.classList.remove("accordion__item_slidedown");
+      elBody.classList.add("collapse");
+      el.classList.add("accordion__item_show");
+      elBody.style["display"] = "";
+      elBody.style["height"] = "";
+      elBody.style["transition"] = "";
+      elBody.style["overflow"] = "";
+    }, this._config.duration);
+  }
+  hide(el) {
+    const elBody = el.querySelector(".accordion__body");
+    if (
+      elBody.classList.contains("collapsing") ||
+      !el.classList.contains("accordion__item_show")
+    ) {
+      return;
+    }
+    elBody.style["height"] = `${elBody.offsetHeight}px`;
+    elBody.offsetHeight;
+    elBody.style["display"] = "block";
+    elBody.style["height"] = 0;
+    elBody.style["overflow"] = "hidden";
+    elBody.style["transition"] = `height ${this._config.duration}ms ease`;
+    elBody.classList.remove("collapse");
+    el.classList.remove("accordion__item_show");
+    elBody.classList.add("collapsing");
+    window.setTimeout(() => {
+      elBody.classList.remove("collapsing");
+      elBody.classList.add("collapse");
+      elBody.style["display"] = "";
+      elBody.style["height"] = "";
+      elBody.style["transition"] = "";
+      elBody.style["overflow"] = "";
+    }, this._config.duration);
+  }
+  toggle(el) {
+    el.classList.contains("accordion__item_show")
+      ? this.hide(el)
+      : this.show(el);
+  }
+}
+
+/*Бургер меню*/
+
+new ItcAccordion(document.querySelector(".accordion"), {
+  alwaysOpen: false,
+});
+
+const hamb = document.querySelector("#hamb");
+const popup = document.querySelector("#popup");
+const body = document.body;
+
+// Клонируем меню, чтобы задать свои стили для мобильной версии
+const menu = document.querySelector("#menu").cloneNode(1);
+
+// При клике на иконку hamb вызываем ф-ию hambHandler
+hamb.addEventListener("click", hambHandler);
+
+// Выполняем действия при клике ..
+function hambHandler(e) {
+  e.preventDefault();
+  // Переключаем стили элементов при клике
+  popup.classList.toggle("open");
+  hamb.classList.toggle("active");
+  body.classList.toggle("noscroll");
+  renderPopup();
+}
+
+//Индикатор прокрутки
+window.onscroll = function () {
+  myFunction();
+};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
